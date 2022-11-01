35aa8de3a7439494eee71b62d4def91b3675fb1a
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-11-01 14:24:40 -0300

    corrige bug do timer e adiciona som

--- src/components/formTimer/index.jsx
+++ src/components/formTimer/index.jsx
@@ -1,38 +1,41 @@
-export const FormTimer = ({tempo, handleChangeTime}) => {
+export const FormTimer = ({ handleChange, start }) => {
 
   const arrHora = Array(11).fill('');
   const arrMinuto = Array(60).fill('');
   const arrSegundo = Array(60).fill('');
 
   return (
-    <form>
+    <form >
       <label htmlFor="hora">
         Horas:
         <select
+          disabled={start}
           name="hora"
           id="hora"
-          onChange={ handleChangeTime }
-          value={ tempo.hora || 0 }>
+          onChange={ handleChange }
+          >
           {arrHora.map((_,index) => <option key={'hora' + index } value={index}>{index}</option>)}
         </select>
       </label>
       <label htmlFor="hora">
         Minutos:
         <select
-          name="minutos"
-          id="minutos"
-          onChange={ handleChangeTime }
-          value={ tempo.minutos || 0 }>
+          disabled={start}
+          name="minuto"
+          id="minuto"
+          onChange={ handleChange }
+          >
           {arrMinuto.map((_,index) => <option key={'minutos' + index } value={index}>{index}</option>)}
         </select>
       </label>
       <label htmlFor="hora">
         Segundos:
         <select
+          disabled={start}
           name="segundo"
           id="segundo"
-          onChange={ handleChangeTime }
-          value={ tempo.segundo || 0 }>
+          onChange={ handleChange }
+          >
           {arrSegundo.map((_,index) => <option key={'segundo' + index } value={index}>{index}</option>)}
         </select>
       </label>
--- src/templates/App.jsx
+++ src/templates/App.jsx
@@ -1,24 +1,80 @@
-import { useState } from 'react';
+import React from 'react';
 import { FormTimer } from '../components/formTimer';
-import { Timer } from '../components/timer';
 import '../styles/App.css';
+import mySound from '../sound/dispertador.mp3';
 
-function App() {
+const ONE_SECOND = 1000;
+const sound = new Audio(mySound);
 
-  const [tempo, setTempo] = useState({});
-
-  const handleChangeTime = ({target}) => {
-    const { name, value } = target;
-    setTempo({ ...tempo, [name]: value });
+export default class App extends React.Component {
+  state = {
+    timer: '00:00:00',
+    hora: 0,
+    minuto: 0,
+    segundo: 0,
+    start: false,
   }
 
-  console.log(tempo);
-  return (
-   <div>
-      <FormTimer handleChangeTime={handleChangeTime} tempo={tempo}/>
-      <Timer />
-   </div>
-  );
+  componentDidUpdate(_, prev) {
+    if (prev.segundo < 0 ) {
+      this.setState((prev) => ({ segundo: 59,
+        start: ((prev.hora > 0)&&(prev.minuto > 0)&&(prev.segundo > 0))
+       }));
+    }
+
+    if (prev.minuto < 0 ) {
+      this.setState( { minuto: 59 });
+    }
+
+    if((prev.hora === 0)&&(prev.minuto === 0)&&(prev.segundo === 0)&&(prev.start)) {
+      this.setState({
+        start: false,
+        hora: 0,
+        minuto: 0,
+        segundo: 0,
+      }, (clearInterval(this.intervalId)));
+      sound.play();
+    }
+  }
+
+  handleAtualizaTimer = () => {
+    const {start} = this.state;
+    if (start) {
+      this.intervalId = setInterval(() => {
+        this.setState((prev) => ({
+          segundo: prev.segundo === 0 ? 59 : prev.segundo -1,
+          minuto: prev.segundo === 0 ? prev.minuto - 1 : prev.minuto,
+          timer: (prev.hora > 9 ? prev.hora : '0' + prev.hora)
+            + ':' + (prev.minuto > 9 ? prev.minuto : '0' + prev.minuto)
+            + ':' + (prev.segundo > 9 ? prev.segundo : '0' + prev.segundo),
+        }));
+      }, ONE_SECOND);
+    }
+  }
+
+  handleStartTimer = () => {
+    const { hora, minuto, segundo } = this.state;
+    if((hora + minuto + segundo) > 0) {
+      this.setState({ start: true }, (this.handleAtualizaTimer));
+    }
+  }
+
+  handleChange = ({target}) => {
+    const { name, value } = target;
+    this.setState({[name]: value});
+  }
+
+
+  render() {
+    const { timer, start } = this.state
+    return (
+      <div>
+        <h1>Timer</h1>
+        <h1>{timer}</h1>
+        <FormTimer handleChange={this.handleChange} start={start} />
+        <button onClick={this.handleStartTimer}>Start</button>
+      </div>
+    )
+  }
 }
 
-export default App;
